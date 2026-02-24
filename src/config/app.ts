d5ba61914a0a714d24533641c0cb50d08fc5ce73
rabin9840 <rabinshrestha984156@gmail.com>
2026-02-24 18:56:58 +0545

    Updated config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,3 +1,4 @@
 const BACKEND_URI = import.meta.env.VITE_BACKEND_URI;
+const PLAN_MODULE_API_URI = import.meta.env.VITE_APP_PLAN_MODULE_API_URI;
 
-export default { BACKEND_URI };
+export default { BACKEND_URI, PLAN_MODULE_API_URI };
